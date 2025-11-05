8d345fd1ff140106d7137cfb8be43bf242193c87
Leonardo Cruz <leonardocruzprimary@gmail.com>
2025-11-05 01:02:28 -0500

    Reverted Util.js back to original. Moved assets folder out of public to to project directory

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,3 @@
-export const getImageUrl = (p) => {
-  if (!p || typeof p !== 'string') {
-    throw new Error(`getImageUrl: invalid path "${p}"`)
-  }
-  // remove any accidental leading slashes to avoid "assets//..."
-  const clean = p.replace(/^\/+/, '')
-  // For files in public/assets
-  return `${import.meta.env.BASE_URL}./assets/${clean}`
-}
+export const getImageUrl = (path) => {
+  return new URL(`assets/${path}`, import.meta.url).href;
+};
