1afbee830ed4f60ae2c8a985c424873878ec2fa6
Leocruzjr <leonardocruzprimary@gmail.com>
2025-12-05 12:33:16 -0500

    Added resume button next to contact button in hero section. On mobile view changes to column.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,9 +7,12 @@ export const Hero = () => {
   <section className={styles.container}>
     <div className={styles.content}>
         <h1 className={styles.title}>Hey, I'm Leonardo</h1>
-            <p className={styles.description}>I'm a front-end developer. I use HTML, CSS, JavaScript and other techs to create responsive websites. See more info below.
+            <p className={styles.description}>I'm a front-end developer. I use JS, HTML, CSS to create responsive websites. See more info below.
             </p>
+            <div className={styles.buttons}>
             <a href="mailto: Leonardocruzprimary@gmail.com" className={styles.contactBtn}>Contact me</a>
+            <a href="https://docs.google.com/document/d/1AaLQy7MK75mCPGzb3YWU9J-YbNEI_aoqXiQGrQssXK0/edit?usp=sharing" target="_blank" className={styles.contactBtn}>Resume</a>
+            </div>
     </div>
     <img
     src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg}/>
